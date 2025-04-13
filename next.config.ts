d33cedf9ea5894d9ee9e87ad8c891875a191d7fb
alexprimak58 <aprimak43@gmail.com>
2025-04-14 01:33:17 +0300

    ignoring eslint and typescript during builds for now

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // REMOVE FOR THE FINAL CHECK BEFORE GOING TO PROD
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
 }
 
 export default nextConfig
