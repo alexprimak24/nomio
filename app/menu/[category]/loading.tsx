79a6ead6a9cc472b438cb149dfdee83392050955
alexprimak58 <aprimak43@gmail.com>
2025-04-14 14:37:29 +0300

    added loading indicator for different pages

---
+++ app/menu/[category]/loading.tsx
@@ -0,0 +1,13 @@
+import { CircularProgress } from '@mui/material'
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className="flex items-center justify-center">
+      <CircularProgress color="success" size="3rem" />
+      <p className="text-xl mt-3">Loading category...</p>
+    </div>
+  )
+}
+
+export default Loading
